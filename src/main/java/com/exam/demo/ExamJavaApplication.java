f9492532aa2c272d9880b7f87bf43988f5b00373
Miguel Fletes <mfletesg@gmail.com>
2024-09-06 12:35:56 -0600

    first commit

---
+++ src/main/java/com/exam/demo/ExamJavaApplication.java
@@ -0,0 +1,13 @@
+package com.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamJavaApplication.class, args);
+	}
+
+}
